28cb456463eaaa15dc27a6d8932b29a33088e78d
Alex S <alex@bold-it.com>
2013-12-10 14:34:24 -0800

    inital

---
+++ Assets/MoveAround.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAround : MonoBehaviour {
+	// Character stats
+	CharacterController controller;
+	float speed = 3.0F;
+	float forwardSpeed = 30.0F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController>();
+
+		// Rotate the sphere around the middle axis with <-/-> and A/D
+		transform.RotateAround(Vector3.zero, Vector3.forward, Input.GetAxis("Horizontal") * speed);
+
+		// Move the sphere forward with ^/v for now
+		Vector3 forward = transform.TransformDirection(Vector3.forward); // Not sure if should use this
+		float curSpeed = forwardSpeed * Input.GetAxis("Vertical");
+
+//		controller.Move(Vector3.forward * forwardSpeed * Time.deltaTime );
+		controller.Move(Vector3.forward * curSpeed * Time.deltaTime );
+
+
+	}
+
+}
+
+/*
+ * var speed : float = 3.0;
+ * var rotateSpeed : float = 3.0;
+ * function Update () {
+ *     var controller : CharacterController = GetComponent(CharacterController);
+ *     // Rotate around y - axis
+ *     transform.Rotate(0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
+ * 
+ *     // Move forward / backward
+ *     var forward : Vector3 = transform.TransformDirection(Vector3.forward);
+ *     var curSpeed : float = speed * Input.GetAxis ("Vertical");
+ *     controller.SimpleMove(forward * curSpeed);
+ * }
+ * @script RequireComponent(CharacterController)
+ */
---
+++ Assets/Standard Assets/Scripts/Camera Scripts/SmoothLookAtDistance.js
@@ -0,0 +1,34 @@
+var target : Transform;
+var damping = 6.0;
+var smooth = true;
+// The distance in the x-z plane to the target
+var distance = 10.0;
+
+
+@script AddComponentMenu("Camera-Control/Smooth Look At")
+
+function LateUpdate () {
+	if (target) {
+		if (smooth)
+		{
+			// Look at and dampen the rotation
+			var rotation = Quaternion.LookRotation(target.position - transform.position);
+			transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+		}
+		else
+		{
+			// Just lookat
+//		    transform.LookAt(target);
+			transform.rotation = Quaternion(0,0,0,0);
+		}
+		
+		transform.position.z = target.position.z - distance;
+
+	}
+}
+
+function Start () {
+	// Make the rigid body not change rotation
+   	if (rigidbody)
+		rigidbody.freezeRotation = true;
+}
